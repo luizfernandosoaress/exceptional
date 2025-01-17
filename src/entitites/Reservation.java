5776d1ed785e1efa905638896298555579de2680
Luiz Fernando da Silva <devpro.luiz@gmail.com>
2025-01-16 21:40:34 -0300

    Good solution

--- src/entitites/Reservation.java
+++ src/entitites/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkin;
@@ -14,7 +16,10 @@ public class Reservation {
 	public Reservation() {
 	}
 
-	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+	public Reservation(Integer roomNumber, Date checkin, Date checkout)  {
+		if (!checkout.after(checkin)) {
+			throw new DomainException("Check-Out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkin = checkin;
 		this.checkout = checkout;
@@ -41,18 +46,17 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkin, Date checkout) {
+	public void updateDates(Date checkin, Date checkout)  {
 		Date now = new Date();
 		if (checkin.before(now) || checkout.before(now)) {
-			return "Reservation dates for updates must be futere dates";
+			 throw new DomainException("Reservation dates for updates must be futere dates");
 		}
 		if (!checkout.after(checkin)) {
-			return "Check-Out date must be after check-in date";
+			throw new DomainException("Check-Out date must be after check-in date");
 		}
-	
-	this.checkin = checkin;
-	this.checkout = checkout;
-	return null;
+		this.checkin = checkin;
+		this.checkout = checkout;
+
 	
 	}
 	
